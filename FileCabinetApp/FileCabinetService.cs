8033a9bd78d3659af2851b4ad61555b815de0eb3
Artem <35197523+dsafsfsgfjnfghsdf@users.noreply.github.com>
2020-05-29 23:30:03 +0300

    Add FileCabnietService.cs file

---
+++ FileCabinetApp/FileCabinetService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileCabinetApp
+{
+    class FileCabinetService
+    {
+        private readonly List<FileCabinetRecord> list = new List<FileCabinetRecord>();
+        public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            // TODO: Добавьте реализацию метода
+            return 0;
+        }
+        public FileCabinetRecord[] GetRecords()
+        {
+            // TODO: Добавьте реализацию метода
+            return new FileCabinetRecord[] { };
+        }
+        public int GetStat()
+        {
+            // TODO: Добавьте реалзиацию метода
+            return 0;
+        }
+    }
+}
